76211c71c0c8eef91f6b0defa476cfe0b08bd313
Michael Bull <mikebull94@gmail.com>
2023-12-11 15:15:33 +0000

    Extract horizontal/vertical dimension functions in day 11

--- src/main/kotlin/com/github/michaelbull/advent2023/day11/Image.kt
+++ src/main/kotlin/com/github/michaelbull/advent2023/day11/Image.kt
@@ -3,6 +3,9 @@ package com.github.michaelbull.advent2023.day11
 import com.github.michaelbull.advent2023.math.Vector2
 import com.github.michaelbull.advent2023.math.manhattanDistance
 
+private typealias Dimension = (Vector2) -> Int
+private typealias Axis = (Int) -> Vector2
+
 fun Sequence<String>.toImage(): Image {
     val galaxies = buildSet {
         for ((y, line) in this@toImage.withIndex()) {
@@ -23,8 +26,8 @@ data class Image(
     val galaxies: Set<Vector2>,
 ) {
 
-    private val galaxiesByColumn = galaxies.sortedBy(Vector2::x)
-    private val galaxiesByRow = galaxies.sortedBy(Vector2::y)
+    private val horizontalGalaxies = galaxies.sortedBy(::horizontal)
+    private val verticalGalaxies = galaxies.sortedBy(::vertical)
 
     fun sumShortestPathLengths(): Long {
         return galaxies
@@ -38,11 +41,11 @@ data class Image(
     }
 
     private fun expandHorizontally(factor: Int): Image {
-        return copy(galaxies = galaxiesByColumn.expand(factor, Vector2::x, ::horizontal))
+        return copy(galaxies = horizontalGalaxies.expand(factor, ::horizontal, ::horizontal))
     }
 
     private fun expandVertically(factor: Int): Image {
-        return copy(galaxies = galaxiesByRow.expand(factor, Vector2::y, ::vertical))
+        return copy(galaxies = verticalGalaxies.expand(factor, ::vertical, ::vertical))
     }
 
     private fun List<Vector2>.expand(factor: Int, dimension: Dimension, axis: Axis): Set<Vector2> {
@@ -68,12 +71,12 @@ data class Image(
         }
     }
 
+    private fun horizontal(vector: Vector2) = vector.x
     private fun horizontal(x: Int) = Vector2(x, 0)
+
+    private fun vertical(vector: Vector2) = vector.y
     private fun vertical(y: Int) = Vector2(0, y)
 }
-
-private typealias Dimension = (Vector2) -> Int
-private typealias Axis = (Int) -> Vector2
 
 private fun <T> Iterable<T>.cartesianProduct(): List<Pair<T, T>> {
     return flatMap { left ->
